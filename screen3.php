4bbe090e7ad6ada650f17333873ff017a9bea9bb
ktracy2 <ktracy2@emich.edu>
2023-04-01 10:24:41 -0400

    Shopping cart displays contents! :)

--- screen3.php
+++ screen3.php
@@ -1,15 +1,26 @@
 
 <!-- Figure 3: Search Result Screen by Prithviraj Narahari, php coding: Alexander Martens -->
 
+
+
 <?php
 
+
+
 session_start();
+
+
 
 include_once 'includes/dbh.inc.php';
 
 
+
+
+
 // Check connection
+
 /*
+
 if ($conn->connect_error) {
 
 echo "MySQL connection failed.";
@@ -19,296 +30,657 @@ echo "MySQL connection failed.";
 echo "MySQL connection success!";
 
 }
+
 */
+
 // Saves the state of the search results page to go back to after looking at review
+
 $queryString = $_SERVER['QUERY_STRING'];
+
 $_SESSION['queryString'] = $queryString;
 
-$cartisbn = $_GET['cartisbn'];
-$_SESSION['cartisbn'] = $cartisbn;
+
+
+
+
+
+
+//check add to cart button is pressed
+
+if(isset($_GET['cartisbn'])) {
+
+	$cartisbn = $_GET['cartisbn'];
+
+
+
+	if(!isset($_SESSION['cart'])) {
+
+		//initialize cart array
+
+		$_SESSION['cart'] = array();
+
+	}
+
+	//push incoming isbn num to cart array
+
+	array_push($_SESSION['cart'], $cartisbn);
+
+}
+
+
+
+// testing purpose 
+
+//echo $_SESSION['cart'];
+
+/* foreach($_SESSION['cart'] as $kk) {
+
+	echo "  ";
+
+  echo $kk;
+
+} */
+
+
+
+// this count the click of add to cart button (not associate to a given book!!)
+
+//$cartSize = count($_SESSION['cart']);
+
+
+
+/* //testing purpose
+
+	echo "\n";
+
+  echo $cartSize;
+
+
+
+//testing purpose --- reset the cart
+
+  if($cartSize > 5)
+
+  unset($_SESSION['cart']);
+ */
+
+
+
+
+
+
+
+
+
+
+
 
 $search = $_GET['searchfor']; //what was in the search text box
+
 //echo $search;
+
 $category = $_GET['category']; //what category was selected, or all
+
 //echo $category;
+
 //Get values saved in searchon[]
+
 $refine = '';
+
 foreach ($_GET['searchon'] as $option){
+
 	//echo "you have selected " . $option; //Each option to refine search, or anywhere
+
 	$refine .= '"'. $option . '"'. ',';
+
 }
+
 $refine = substr($refine, 0, strlen($refine) - 1);
+
+
 
 //echo $refine;
 
+
+
 if(str_contains($refine, "\"title\"")){
+
 	$sql = "SELECT * FROM book WHERE title LIKE '%$search%'";
+
 	$result = mysqli_query($conn, $sql);
+
 		$_SESSION['search_results'] = array();
+
+
 
 		if (mysqli_num_rows($result) > 0) {
 
+
+
 			while ($row = mysqli_fetch_assoc($result)) {
 
+
+
 				$_SESSION['search_results'][] = $row;
+
 				
+
 			}
+
 		}
+
 }
+
 if(str_contains($refine, "\"author\"")){
+
 	$sql = "SELECT * FROM book WHERE author LIKE '%$search%'";
+
 	$result = mysqli_query($conn, $sql);
+
 		$_SESSION['search_results'] = array();
+
+
 
 		if (mysqli_num_rows($result) > 0) {
 
+
+
 			while ($row = mysqli_fetch_assoc($result)) {
 
+
+
 				$_SESSION['search_results'][] = $row;
+
 				
+
 			}
+
 		}
+
 }if(str_contains($refine, "\"publisher\"")){
+
 	$sql = "SELECT * FROM book WHERE publisher LIKE '%$search%'";
+
 	$result = mysqli_query($conn, $sql);
+
 		$_SESSION['search_results'] = array();
+
+
 
 		if (mysqli_num_rows($result) > 0) {
 
+
+
 			while ($row = mysqli_fetch_assoc($result)) {
 
+
+
 				$_SESSION['search_results'][] = $row;
+
 				
+
 			}
+
 		}
+
 }
+
 if(str_contains($refine, "\"isbn\"")){
+
 	$sql = "SELECT * FROM book WHERE isbn LIKE '%$search%'";
+
 	$result = mysqli_query($conn, $sql);
+
 		$_SESSION['search_results'] = array();
+
+
 
 		if (mysqli_num_rows($result) > 0) {
 
+
+
 			while ($row = mysqli_fetch_assoc($result)) {
 
+
+
 				$_SESSION['search_results'][] = $row;
+
 				
+
 			}
+
 		}
+
 }
+
 ///////////////////////////////////////////////////////////////////////////////////
+
 //IF THERE IS NO CATEGORY SELECTED AND SEARCH ANYWHERE IS SELECTED, LIST ALL BOOKS IN DB
+
 if ($refine == "\"anywhere\"" && $category == 'all'){
+
 		$search = $_GET['searchfor'];
+
 		//echo $search;
+
 		$sql = "SELECT * FROM book WHERE title LIKE '%$search%'";
+
 		$result = mysqli_query($conn, $sql);
+
 		$_SESSION['search_results'] = array();
+
+
 
 		if (mysqli_num_rows($result) > 0) {
 
+
+
 			while ($row = mysqli_fetch_assoc($result)) {
 
+
+
 				$_SESSION['search_results'][] = $row;
+
 				
+
 			}
+
 		}
 
+
+
 }
+
 //////////////////////////////////////////////////////////////////////////////////
+
 //if Category is selected, build query that refines search including category
 
+
+
 else if ($category != 'all') {
+
 		$sql = "SELECT * FROM book WHERE category = '$category';";
+
 		$result = mysqli_query($conn, $sql);
+
 		
+
 		$_SESSION['search_results'] = array();
+
+
 
 		if (mysqli_num_rows($result) > 0) {
 
+
+
 			while ($row = mysqli_fetch_assoc($result)) {
 
+
+
 				$_SESSION['search_results'][] = $row;
+
 				
+
 			}
+
 		}
 
+
+
 }
+
+
 
 // // Add item to cart --- i think this part should be in shopping_cart
 
+
+
 // if (isset($_GET['cartisbn'])) {
+
+
 
 //     $cart_item = array(
 
+
+
 //         'isbn' => $_GET['cartisbn'],
+
+
 
 //         'title' => $_GET['carttitle'],
 
+
+
 //         'price' => $_GET['cartprice']
+
+
 
 //     );
 
+
+
 //     $_SESSION['cart'][] = $cart_item;
+
+
 
 // }
 
+
+
 // Close database connection
+
+
 
 mysqli_close($conn);
 
+
+
 ?>
+
+
 
 <!-- Figure 3: Search Result Screen by Prithviraj Narahari, php coding: Alexander Martens -->
 
+
+
 <html>
+
+
 
 <head>
 
+
+
 	<title> Search Result - 3-B.com </title>
+
 	<link rel="stylesheet" href="styles.css">
+
 	<script>
+
+
 
 	//redirect to reviews page
 
+
+
 	function review(isbn, title){
+
+
 
 		window.location.href="screen4.php?isbn="+ isbn + "&title=" + title;
 
+
+
 	}
+
+
 
 	//add to cart
 
+
+
 	function cart(isbn, searchfor, searchon, category){
+
+
 
 		window.location.href="screen3.php?cartisbn="+ isbn + "&searchfor=" + searchfor + "&searchon=" + searchon + "&category=" + category;
 
+
+
 	}
+
+
 
 	</script>
 
+
+
 </head>
 
+
+
 <body>
+
 	<h1 align = "center">Search Results</h1>
+
 	<table align="center" style="border:1px solid blue;">
 
+
+
 		<tr>
+
+
 
 			<td align="left">
 
+
+
 					<?php 
+
 					// Get a count of all items in shopping cart and set equal to below 
+
 					$numItems = 1;
+
 					
+
 					?>
+
+
 
 					<h6> <fieldset>Your Shopping Cart has <?php echo $numItems?> items</fieldset> </h6>
 
+
+
 				
 
+
+
 			</td>
+
+
 
 			<td>
 
+
+
 				&nbsp
 
+
+
 			</td>
+
+
 
 			<td align="right">
 
+
+
 				<form action="shopping_cart.php" method="post">
+
+
 
 					<input type="submit" value="Manage Shopping Cart">
 
+
+
 				</form>
 
+
+
 			</td>
+
+
 
 		</tr>	
 
+
+
 		<tr>
+
+
 
 		<td style="width: 350px" colspan="3" align="center">
 
+
+
 			<div id="bookdetails" style="overflow:scroll;height:180px;width:400px;border:1px solid black;background-color:LightBlue">
+
+
 
 			<table>
 
+
+
 			<?php
+
+
 
 			// Display search results
 
+
+
 			foreach ($_SESSION['search_results'] as $row) {
+
 				
+
 				echo "<tr>";
+
+
 
 				echo "<td align='left'><button name='btnCart' id='btnCart' onClick='cart(\"".$row['isbn']."\", \"".$search."\", \"Array\", \"all\")'>Add to Cart</button></td>";
 
+
+
 				echo "<td rowspan='2' align='left'>".$row['title']."<br>By ".$row['author']."<br><b>Publisher:</b> ".$row['publisher'].",<br><b>ISBN:</b> ".$row['isbn']." <b>Price:</b> ".$row['price']."</td>";
 
+
+
 				echo "</tr>";
+
+
 
 				echo "<tr>";
 
+
+
 				echo "<td align='left'><button name='review' id='review' onClick='review(\"".$row['isbn']."\", \"".$row['title']."\")'>Reviews</button></td>";
+
+
 
 				echo "</tr>";
 
+
+
 				echo "<tr><td colspan='2'><p>_______________________________________________</p></td></tr>";
+
+
 
 			}
 
+
+
 		?>
+
+
 
 						
 
+
+
 		    </table>
+
+
 
 			</div>
 
+
+
 			
+
+
 
 		</td>
 
+
+
 		</tr>
+
+
 
 		<tr>
 
+
+
 			<td align= "center">
+
+
 
 				<form action="confirm_order.php" method="get">
 
+
+
 					<input type="submit" value="Proceed To Checkout" id="checkout" name="checkout">
+
+
 
 				</form>
 
+
+
 			</td>
 
+
+
 			<td align="center">
+
+
 
 				<form action="screen2.php" method="post">
 
+
+
 					<input type="submit" value="New Search">
+
+
 
 				</form>
 
+
+
 			</td>
+
+
 
 			<td align="center">
 
+
+
 				<form action="index.php" method="post">
+
+
 
 					<input type="submit" name="exit" value="EXIT 3-B.com">
 
+
+
 				</form>
+
+
 
 			</td>
 
+
+
 		</tr>
+
+
 
 	</table>
 
+
+
 </body>
+
+
 
 </html>
--- shopping_cart.php
+++ shopping_cart.php
@@ -1,79 +1,212 @@
 <?php
+
 include_once 'includes/dbh.inc.php';
-// Check connection
-/* if ($conn->connect_error) {
-echo "MySQL connection failed.";
-} else {
-echo "MySQL connection success!";
-} */
+
+
 	// Start a session 
+
 	session_start();
+
 	//$_SESSION["user"] = "default";
+
 	$user = $_SESSION["user"];
+
 	echo $user;
+
 	//until user logs in/creates an account, cart_id is for the default temporary user
+
 	$_SESSION["cart_id"] = "temp_user";
+
 	$_SESSION["cart_contents"] = $temp_cart;
-	echo $_SESSION['cartisbn'];
- 	
+/* 
+						//get unique isbn
+
+						$_SESSION['cart_query'] = array_unique($_SESSION['cart']);
+
+						foreach($_SESSION['cart_query'] as $id){
+
+							$query = "SELECT * FROM book WHERE isbn = '$id'";
+
+							$result = mysqli_query($conn, $query);
+
+
+							// Display book information
+
+							while ($row = mysqli_fetch_assoc($result)) {
+
+								echo '<tr><td><button name=\'delete\' id=\'delete\' onClick=\'del("123441")\';return false;>Delete Item</button></td>';
+						echo '<td>' .  $row['title'] . '</br><b>By </b>' . $row['author'] . '</br><b>Publisher: </b>' . $row['publisher'] . '</td><td><input id=\'txt123441\' name=\'txt123441\' value=\'1\' size=\'1\' /></td><td>' . $row['price'] . '</td></tr>';
+
+								echo "Title: " . $row['title'] . "<br>";
+
+								echo "Author: " . $row['author'] . "<br>";
+
+								echo "ISBN: " . $row['isbn'] . "<br>";
+
+								echo "Price: $" . $row['price'] . "<br><br>";
+
+							}
+
+					 } */
+
+
+
+
 	
+
+
+
+
+
+
+
+
+
+
+
+	
+
 ?>
+
 <!DOCTYPE HTML>
+
 <head>
+
 	<title>Shopping Cart</title>
+
 	<link rel="stylesheet" href="styles.css">
+
 	<script>
+
 	//remove from cart
+
 	function del(isbn){
+
 		window.location.href="shopping_cart.php?delIsbn="+ isbn;
+
 	}
+
 	</script>
+
 </head>
+
 <body>
+
 	<h1 align = "center">Shopping Cart</h1>
+
 	<table align="center" style="border:2px solid blue;">
 
+
+
 		<!-- Show user name on cart, if user is not logged in, username is "default" -->
+
 		<?php
+
 		print "<tr><td>User: $user</td><td></td><td></td></tr>";
+
 		?>
 
+
+
 		<tr>
+
 			<td align="center">
+
 				<form id="checkout" action="confirm_order.php" method="get">
+
 					<input type="submit" name="checkout_submit" id="checkout_submit" value="Proceed to Checkout">
+
 				</form>
+
 			</td>
+
 			<td align="center">
+
 				<form id="new_search" action="screen2.php" method="post">
+
 					<input type="submit" name="search" id="search" value="New Search">
+
 				</form>								
+
 			</td>
+
 			<td align="center">
+
 				<form id="exit" action="index.php" method="post">
+
 					<input type="submit" name="exit" id="exit" value="EXIT 3-B.com">
+
 				</form>					
+
 			</td>
+
 		</tr>
+
 		<tr>
+
 				<form id="recalculate" name="recalculate" action="" method="post">
+
 			<td  colspan="3">
+
 				<div id="bookdetails" style="overflow:scroll;height:180px;width:400px;border:1px solid black;">
+
 					<table align="center" BORDER="2" CELLPADDING="2" CELLSPACING="2" WIDTH="100%">
+
 						<th width='10%'>Remove</th><th width='60%'>Book Description</th><th width='10%'>Qty</th><th width='10%'>Price</th>
-						<tr><td><button name='delete' id='delete' onClick='del("123441");return false;'>Delete Item</button></td><td><?php echo $title ?></br><b>By</b> <?php echo $author ?></br><b>Publisher:</b> <?php echo $publisher ?></td><td><input id='txt123441' name='txt123441' value='1' size='1' /></td><td><?php echo $price ?></td></tr>					</table>
+
+						<?php
+
+						//get unique isbn
+
+						$_SESSION['cart_query'] = array_unique($_SESSION['cart']);
+
+						foreach($_SESSION['cart_query'] as $id){
+
+							$query = "SELECT * FROM book WHERE isbn = '$id'";
+
+							$result = mysqli_query($conn, $query);
+
+
+							// Display book information
+
+							while ($row = mysqli_fetch_assoc($result)) {
+
+								echo '<tr><td><button name=\'delete\' id=\'delete\' onClick=\'del("123441")\';return false;>Delete Item</button></td>';
+								echo '<td>' .  $row['title'] . '</br><b>By </b>' . $row['author'] . '</br><b>Publisher: </b>' . $row['publisher'] . '</td><td><input id=\'txt123441\' name=\'txt123441\' value=\'1\' size=\'1\' /></td><td>' . $row['price'] . '</td></tr>';
+
+							}
+
+						}
+						
+						?>
+						
+					</table>
 				</div>
+
 			</td>
+
 		</tr>
+
 		<tr>
+
 			<td align="center">				
+
 					<input type="submit" name="recalculate_payment" id="recalculate_payment" value="Recalculate Payment">
+
 				</form>
+
 			</td>
+
 			<td align="center">
+
 				&nbsp;
+
 			</td>
+
 			<td align="center">Subtotal:  $12.99</td>
+
 		</tr>
+
 	</table>
+
 </body>
